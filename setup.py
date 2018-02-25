dea4fde0416133a12279bcfd5e72f1673a2f7d40
foospidy <info@deceptionlogic.com>
2018-02-25 14:01:02 -0500

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(HERE, 'README.rst')) as f:
 
 setup(
     name="deceptionlogic",
-    version="0.0.4",
+    version="0.0.5",
     author="Deception Logic, Inc.",
     author_email="info@deceptionlogic.com",
     description=("A python wrapper and CLI tool for the Deception Logic API."),
